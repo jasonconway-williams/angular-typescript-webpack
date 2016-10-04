65f8218e5c70b607d369514fafcb6a4037ee1694
jason conway-williams <jasonconwaywilliams>
2016-10-04 23:57:39 +0100

    Created some es6 js files to test the karma configuration.

--- src/test.ts
+++ src/test.ts
@@ -2,6 +2,7 @@
 require('phantomjs-polyfill')
 
 requireAll((<any>require).context("./", true, /spec.ts$/));
+requireAll((<any>require).context("./", true, /spec.js$/));
 function requireAll(r: any): any {
     r.keys().forEach(r);
 }
--- webpack/loaders.js
+++ webpack/loaders.js
@@ -1,6 +1,14 @@
 module.exports = [
     {test: /\.ts(x?)$/, loader: 'ts-loader'},
     {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader', // 'babel-loader' is also a valid name to reference
+        query: {
+            presets: ['es2015']
+        }
+    },
+    {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
     },
